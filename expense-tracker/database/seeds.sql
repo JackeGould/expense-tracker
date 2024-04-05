40d2af503d9ff8cae286b2b474af400bff390495
RutCoding226 <154572480+RutCoding226@users.noreply.github.com>
2024-04-04 17:23:48 -0700

    Create seeds.sql

---
+++ expense-tracker/database/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO expense_values (id, expense_item_name, expense_item_amount )
+VALUES (001, "plant", 50),
+       (002, "chair", 100),
+       (003, "light", 75),
+       (004, "table", 150),
+       (005, "painting", 200);
+
+INSERT INTO expense_description (id, expense_description_id, expense_description)
+VALUES (001, 01, "potted plant for display").
+       (002, 02, "Strong reclining chair"),
+       (003, 03, "table lamo"),
+       (004, 04, "Four Cornered deskspace"),
+       (005, 05, "Wall painting decorated by mational artists");
